4bfdc5ab78a09f7590d65f61b091010dc908d396
ramprits <rampritsahani@outlook.in>
2019-02-10 12:29:56 +0530

    styled component aded

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,12 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      }
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
